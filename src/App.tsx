785ca523c27823923715cbe1c725ef7afd8655d4
sunlejin <sunlejin@bytedance.com>
2023-08-23 22:15:41 +0800

    feat: 滚动日历

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate, NavLink } from "react-router-dom";
 import SimpleCalendar from "./pages/simple-calendar";
 import ScrollCalendar from "./pages/scroll-calendar";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/simple-calendar" Component={SimpleCalendar} />
         <Route path="/scroll-calendar" Component={ScrollCalendar} />
         <Route path="*" element={<Navigate to="/simple-calendar" replace />} />
       </Routes>
-      <div>
-        <a href="/simple-calendar">简单日历</a>
-        <a href="/scroll-calendar">滚动日历</a>
+      <div style={{ position: "fixed", top: 64, right: 64 }}>
+        <NavLink to="/simple-calendar">简单日历</NavLink>
+        <NavLink to="/scroll-calendar">滚动日历</NavLink>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
